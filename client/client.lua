a1d1e6b99217de38395cdd567a5262be380a0490
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2023-02-21 19:01:38 -0500

    tweak(client): wrap vehicle spawning in a thread

--- client/client.lua
+++ client/client.lua
@@ -13,13 +13,15 @@ local function getVehicleType(model)
 end
 
 local function spawnVehicle(model, coords, properties)
-    model = type(model) == "string" and joaat(model) or model
-    local modelType = getVehicleType(model)
-    local hash = lib.callback.await(Shared.generateHash, false)
-    local hashTable = LocalPlayer.state[Shared.hashTable] or {}
-    hashTable[hash] = hash
-    LocalPlayer.state:set(Shared.hashTable, hashTable, true)
-    TriggerServerEvent(Shared.spawnVehicleEvent, model, modelType, coords, properties, hash)
+    CreateThread(function()
+        model = type(model) == "string" and joaat(model) or model
+        local modelType = getVehicleType(model)
+        local hash = lib.callback.await(Shared.generateHash, false)
+        local hashTable = LocalPlayer.state[Shared.hashTable] or {}
+        hashTable[hash] = hash
+        LocalPlayer.state:set(Shared.hashTable, hashTable, true)
+        TriggerServerEvent(Shared.spawnVehicleEvent, model, modelType, coords, properties, hash)
+    end)
 end
 
 lib.callback.register(Shared.getVehicleType, function(model)
@@ -37,3 +39,22 @@ AddEventHandler("onResourceStop", function(resource)
     if resource ~= Shared.currentResourceName then return end
     LocalPlayer.state:set(Shared.hashTable, nil, true)
 end)
+
+--[[
+-- test
+CreateThread(function()
+    Wait(5000)
+    local vehicles = {
+        ["lwgtr"] = vector4(119.35, -930.5, 29.8, 162.34),
+        ["lwgtr2"] = vector4(116.96, -937.68, 29.72, 161.87),
+        ["adder"] = vector4(112.98, -949.65, 29.59, 161.63),
+        ["tenf"] = vector4(109.68, -958.95, 29.47, 161.02),
+        ["tenf2"] = vector4(105.92, -971.01, 29.36, 163.7),
+    }
+    
+    for model, coords in pairs(vehicles) do
+        -- print(model, coords)
+        exports[Shared.currentResourceName]:spawnVehicle(model, coords)
+    end
+end)
+]]
